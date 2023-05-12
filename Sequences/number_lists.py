dba048c5dd55b5d152b46c6433530c94af19c8ca
davestroud <david@davidstroud.me>
2023-05-12 12:14:57 -0400

    add new code to numbers list

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -2,11 +2,21 @@ empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-numbers = [even, odd]
-print(numbers)
+even.extend(odd)
+print(even)
+another_even = even
+print(another_even)
 
-for number_list in numbers:
-    print(number_list)
+even.sort(reverse=True)
+print(even)
+print(another_even)
 
-    for value in number_list:
-        print(value)
+
+# numbers = [even, odd]
+# print(numbers)
+
+# for number_list in numbers:
+#     print(number_list)
+
+#     for value in number_list:
+#         print(value)
