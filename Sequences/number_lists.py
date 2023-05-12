e8d5d5beb123e694416ebd4fe01c0f57be219d24
davestroud <david@davidstroud.me>
2023-05-12 13:16:16 -0400

    add drawio demo file

--- Sequences/number_lists.py
+++ Sequences/number_lists.py
@@ -2,21 +2,21 @@ empty_list = []
 even = [2, 4, 6, 8]
 odd = [1, 3, 5, 7, 9]
 
-even.extend(odd)
-print(even)
-another_even = even
-print(another_even)
+# even.extend(odd)
+# print(even)
+# another_even = even
+# print(another_even)
 
-even.sort(reverse=True)
-print(even)
-print(another_even)
+# even.sort(reverse=True)
+# print(even)
+# print(another_even)
 
 
-# numbers = [even, odd]
-# print(numbers)
+numbers = [even, odd]
+print(numbers)
 
-# for number_list in numbers:
-#     print(number_list)
+for number_list in numbers:
+    print(number_list)
 
-#     for value in number_list:
-#         print(value)
+    for value in number_list:
+        print(value)
